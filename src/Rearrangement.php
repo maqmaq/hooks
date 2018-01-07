256491be28d70d7055c45f914f0d5f84c46f1989
maqmaq <m.mmackows@gmail.com>
2018-01-07 14:32:06 +0100

    run hook

--- src/Rearrangement.php
+++ src/Rearrangement.php
@@ -4,57 +4,53 @@
 class Rearrangement
 {
     use SomeTrait;
+    public const PUBLIC_CONST = 1;
+
+    protected const PROTECTED_CONST = 2;
+    private const PRIVATE_CONST = 3;
 
     public static $publicStaticProperty;
 
+    public $publicField;
+
     protected static $protectedStaticProperty;
+    protected $protectedField;
 
     private static $privateStaticProperty;
+    private $privateField;
 
     /**
      * Rearrangement constructor.
      */
     public function __construct()
     {
-
-        
     }
 
-    public static function publicStaticFunction() {
-
-    }
-
-    protected static function protectedStaticFunction() {
-
-    }
-
-    private static function privateStaticFunction() {
-
-    }
-
-    function __destruct()
+    public function __destruct()
     {
     }
 
-
-    public $publicField;
-    private $privateField;
-    protected $protectedField;
-
-    protected const PROTECTED_CONST = 2;
-    public const PUBLIC_CONST = 1;
-    private const PRIVATE_CONST = 3;
-
-    function __call($name, $arguments)
+    public function __call($name, $arguments)
     {
     }
 
+    public static function publicStaticFunction()
+    {
+    }
 
     public function publicFunction()
     {
     }
 
+    protected static function protectedStaticFunction()
+    {
+    }
+
     protected function protectedFunction()
+    {
+    }
+
+    private static function privateStaticFunction()
     {
     }
 
