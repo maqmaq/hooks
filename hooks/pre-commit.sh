f29a58d560610ea588a887f44616c5bbe072ed71
maqmaq <m.mmackows@gmail.com>
2018-01-07 13:43:18 +0100

    run hook

--- hooks/pre-commit.sh
+++ hooks/pre-commit.sh
@@ -9,7 +9,7 @@ fi
   file=${file}
   if [[ $file = *.php ]];
   then
-    $fixer_path fix "$file" --rules=@PSR2,blank_line_before_statement,no_extra_consecutive_blank_lines
+    $fixer_path fix "$file" --rules=@PSR2,blank_line_before_statement,no_extra_consecutive_blank_lines,ordered_class_elements
     git add "$file"
   fi
 done < <(git diff --name-only --cached --diff-filter=ACMRTUXB)
