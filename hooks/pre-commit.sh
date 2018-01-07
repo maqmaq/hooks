d6bef01ccee9829009a05ddbbe2e1b3749ff0d84
maqmaq <m.mmackows@gmail.com>
2018-01-07 14:20:23 +0100

    fixed composer command

--- hooks/pre-commit.sh
+++ hooks/pre-commit.sh
@@ -9,7 +9,7 @@ fi
   file=${file}
   if [[ $file = *.php ]];
   then
-    $fixer_path fix "$file"
+    $fixer_path fix "$file" --config=.php_cs.dist
     git add "$file"
   fi
 done < <(git diff --name-only --cached --diff-filter=ACMRTUXB)
