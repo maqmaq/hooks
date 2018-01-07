a0163d5cfc817dd97f4cc577339b37cda340b914
maqmaq <m.mmackows@gmail.com>
2018-01-07 13:40:26 +0100

    run hook

---
+++ src/Rearrangement.php
@@ -0,0 +1,17 @@
+<?php
+
+
+class Rearrangement
+{
+    public function publicFunction()
+    {
+    }
+
+    protected function protectedFunction()
+    {
+    }
+
+    private function privateFunction()
+    {
+    }
+}
